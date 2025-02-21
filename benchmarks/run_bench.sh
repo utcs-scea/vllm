ec50298c6804fb8fe007520e79411a24c8b1f430
Taeklim Kim <tlkim@c636-122.vista.tacc.utexas.edu>
2025-02-21 16:10:26 -0600

    Added serving bench script and results for iscal rebuttal

--- benchmarks/run_bench.sh
+++ benchmarks/run_bench.sh
@@ -1,33 +1,51 @@
 #!/bin/bash
 
-models=('meta-llama/Llama-2-7b-chat-hf')
 models=('meta-llama/Llama-2-13b-hf')
-models=('google/gemma-2-9b')
 models=('mistralai/Mistral-7B-v0.1' 'meta-llama/Llama-2-7b-chat-hf' 'meta-llama/Llama-2-13b-hf' 'google/gemma-2-9b')
+models=('facebook/opt-125m')
+models=('google/gemma-2-9b')
+models=('deepseek-ai/deepseek-llm-7b-chat')
+models=('meta-llama/Llama-2-7b-chat-hf')
+models=('deepseek-ai/deepseek-llm-7b-chat')
+models=('openai-community/gpt2-xl')
+models=('meta-llama/Llama-2-13b-hf')
 models=('meta-llama/Llama-3.1-8B')
+models=('mistralai/Mistral-7B-v0.1')
+models=('mistralai/Mistral-7B-v0.1' 'meta-llama/Llama-3.1-8B' 'meta-llama/Llama-2-13b-hf' 'openai-community/gpt2-xl' 'deepseek-ai/deepseek-llm-7b-chat')
+models=('meta-llama/Llama-2-13b-hf' 'openai-community/gpt2-xl' 'deepseek-ai/deepseek-llm-7b-chat')
 
-gpu_mem=('0.9' '0.85' '0.8' '0.75' '0.7')
-gpu_mem=('0.9')
+gpu_mem=('0.95' '0.9' '0.85' '0.8' '0.75' '0.7' '0.65' '0.6' '0.55' '0.5' '0.45' '0.4' '0.35' '0.3' '0.25' '0.2')
+gpu_mem=('0.9' '0.8' '0.7' '0.6' '0.5' '0.4' '0.3')
+gpu_mem=('0.95')
 
 device_num=0
 
-output_dir=$(pwd)/results_throughput
+#output_dir=$(pwd)/results_throughput
+output_dir=$(pwd)/results_peak_usage
+export HF_HOME=/work/10000/tlkim/hf_cache
 
 for model in "${models[@]}"
 do 
     for gpu_mem_util in "${gpu_mem[@]}"
     do
-        export CUDA_VISIBLE_DEVICES=$(($device_num%2))
-        python benchmark_throughput.py \
+        #export CUDA_VISIBLE_DEVICES=$(($device_num%2))
+        python3 benchmark_throughput.py \
             --backend vllm \
             --dataset $(pwd)/ShareGPT_V3_unfiltered_cleaned_split.json \
             --model ${model} \
             --swap-space 0 \
             --gpu-memory-utilization ${gpu_mem_util} \
             --num-prompts 1000 \
-            --output-json $output_dir/${gpu_mem_util}.json 
+            --output-json $output_dir/${model}-${gpu_mem_util}.json |& tee $output_dir/${model}-${gpu_mem_util}.log
+        echo -n "${gpu_mem_util} " >> $output_dir/${model}-peak-cache-usage.log
+        grep "Throughput:" $output_dir/${model}-${gpu_mem_util}.log | cut -f2 -d ' ' | tr '\n' ' ' >> $output_dir/${model}-peak-cache-usage.log 
+        grep -oP " GPU KV cache usage:\s+\K\w+" $output_dir/${model}-${gpu_mem_util}.log | sort -n | tail -n 1 >> $output_dir/${model}-peak-cache-usage.log
         ((device_num++))
         echo "Done, sleeping 10 seconds for cooling down GPU"
         sleep 10
     done
 done
+
+
+            #--max-model-len 22464 \
+           # --max-model-len 28240 \
--- benchmarks/run_serving.sh
+++ benchmarks/run_serving.sh
@@ -1,17 +1,68 @@
 #!/bin/bash
 
+models=('meta-llama/Llama-2-13b-hf')
+models=('mistralai/Mistral-7B-v0.1' 'meta-llama/Llama-2-7b-chat-hf' 'meta-llama/Llama-2-13b-hf' 'google/gemma-2-9b')
+models=('facebook/opt-125m')
+models=('google/gemma-2-9b')
+models=('deepseek-ai/deepseek-llm-7b-chat')
 models=('meta-llama/Llama-2-7b-chat-hf')
+models=('deepseek-ai/deepseek-llm-7b-chat')
+models=('openai-community/gpt2-xl')
+models=('meta-llama/Llama-2-13b-hf')
+models=('meta-llama/Llama-3.1-8B')
+models=('mistralai/Mistral-7B-v0.1')
+models=('mistralai/Mistral-7B-v0.1' 'meta-llama/Llama-3.1-8B' 'meta-llama/Llama-2-13b-hf' 'openai-community/gpt2-xl' 'deepseek-ai/deepseek-llm-7b-chat')
+models=('meta-llama/Llama-3.1-8B')
+models=('openai-community/gpt2-xl' 'deepseek-ai/deepseek-llm-7b-chat' 'meta-llama/Llama-2-13b-hf')
+models=('deepseek-ai/deepseek-llm-7b-chat' 'meta-llama/Llama-2-13b-hf')
+models=('meta-llama/Llama-2-13b-hf')
 
-export CUDA_VISIBLE_DEVICES=0
+gpu_mem=('0.95' '0.9' '0.85' '0.8' '0.75' '0.7' '0.65' '0.6' '0.55' '0.5' '0.45' '0.4' '0.35' '0.3' '0.25' '0.2')
 
-python benchmark_serving.py \
-    --backend vllm \
-    --dataset-path /home/tkim/workspace/vllm/benchmarks/ShareGPT_V3_unfiltered_cleaned_split.json \
-    --model meta-llama/Llama-2-7b-chat-hf \
-    --num-prompts 1000 \
-    --request-rate 1 \
-    --result-filename $models-1qps-0.9mem \
-    --result-dir /home/tkim/workspace/vllm/benchmarks/result_serving
-#    --swap-space 0 \
-#    --gpu-memory-utilization 0.9 \
+gpu_mem=('0.9' '0.8' '0.7' '0.6' '0.5' '0.4' '0.3')
+request_rates=('1' '16' 'inf')
 
+device_num=0
+
+#output_dir=$(pwd)/results_throughput
+output_dir=$(pwd)/results_serving
+export HF_HOME=/work/10000/tlkim/hf_cache
+
+for model in "${models[@]}"
+do 
+    for gpu_mem_util in "${gpu_mem[@]}"
+    do
+        for request_rate in "${request_rates[@]}"
+        do
+            vllm serve ${model} \
+                --swap-space 0 \
+                --disable-log-requests \
+                --gpu-memory-utilization ${gpu_mem_util} |& tee $output_dir/${model}-${gpu_mem_util}-${request_rate}-server.log &
+            echo "Waiting for launching server..."
+            sleep 200
+
+            python3 benchmark_serving.py \
+                --backend vllm \
+                --dataset-name sharegpt \
+                --dataset-path $(pwd)/ShareGPT_V3_unfiltered_cleaned_split.json \
+                --model ${model} \
+                --request-rate ${request_rate} \
+                --num-prompts 1000 |& tee $output_dir/${model}-${gpu_mem_util}-${request_rate}-client.log
+            echo -n "${gpu_mem_util} " >> $output_dir/${model}.log
+            sleep 5
+            grep "Mean TTFT" $output_dir/${model}-${gpu_mem_util}-${request_rate}-client.log | tr -s ' ' | cut -f4 -d ' ' | tr '\n' ' ' >>  $output_dir/${model}.log
+            grep "P99 TTFT"  $output_dir/${model}-${gpu_mem_util}-${request_rate}-client.log | tr -s ' ' | cut -f4 -d ' ' | tr '\n' ' ' >>  $output_dir/${model}.log
+            grep "Mean TPOT" $output_dir/${model}-${gpu_mem_util}-${request_rate}-client.log | tr -s ' ' | cut -f4 -d ' ' | tr '\n' ' ' >>  $output_dir/${model}.log
+            grep "P99 TPOT"  $output_dir/${model}-${gpu_mem_util}-${request_rate}-client.log | tr -s ' ' | cut -f4 -d ' ' | tr '\n' ' ' >>  $output_dir/${model}.log
+            grep -oP " GPU KV cache usage:\s+\K\w+" $output_dir/${model}-${gpu_mem_util}-${request_rate}-server.log | sort -n | tail -n 1 >> $output_dir/${model}.log
+            echo "Done, sleeping 10 seconds for cooling down GPU"
+            sleep 10
+            pkill vllm
+            echo "Killing vllm server..."
+            sleep 10
+        done
+    done
+done
+
+            #--max-model-len 22464 \
+           # --max-model-len 28240 \
